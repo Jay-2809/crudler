a44bf36c02f3e8584e54adbe38e5b0aa4de0ab5f
Jay-2809 <jayparmar2809@yahoo.com>
2025-11-23 14:36:34 +0000

    user form,list,view and ltem

--- src/components/entity/modules/ModuleForm.js
+++ src/components/entity/modules/ModuleForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet } from "react-native";
 import useLoad from "../../API/useLoad.js";
 import Icons from "../../UI/Icons.js";
 import Form from "../../UI/Form.js";
